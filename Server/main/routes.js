448e852388c4a06fe96ce5a04e3f214637114088
samira arabgol <samiraarabgol@samiras-MacBook-Pro.local>
2019-10-06 21:49:25 -0500

    building first round of client and server

---
+++ Server/main/routes.js
@@ -0,0 +1,18 @@
+var express = require('express')
+let data;
+var dbinfo = require('../passes')
+const { Pool } = require('pg')
+const pool = new Pool(dbinfo)
+pool.query('SELECT * from users', (err, res) => {
+    if(!err) {data = res}
+  console.log(err, res)
+  pool.end()
+})
+
+var router = express.Router()
+
+router.get('/user', function(req, res) {
+    res.json(data)
+})
+
+module.exports = router
