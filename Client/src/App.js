ac7d1dcb623058063b2ed88b0fa9b5130555b6b7
samira arabgol <samiraarabgol@samiras-MacBook-Pro.local>
2019-10-20 20:48:24 -0500

    add the clinet

---
+++ Client/src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [value, setValue] = useState(null)
+  useEffect(()=>{
+    axios.get('/user')
+      .then(res => {
+        console.log(res.data.rows, "????")
+        setValue(res.data.rows)
+      })
+      .catch(err => console.log(err))
+      })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>{JSON.stringify(value)}</div>
+        {value && value.map((row, index) => 
+          <div key={index}>{row.firstname}</div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
